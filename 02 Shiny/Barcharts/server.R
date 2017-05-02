d3a5e1652f17a27738824817c0bd6a540ddcde73
Kurt Kranz <kurtakranz@gmail.com>
2017-05-02 08:21:22 -0500

    finished shiny app

--- 02 Shiny/Barcharts/server.R
+++ 02 Shiny/Barcharts/server.R
@@ -2,11 +2,13 @@
 require(ggplot2)
 require(dplyr)
 require(shiny)
+require(plotly)
 require(shinydashboard)
 require(data.world)
 require(readr)
 require(DT)
 require(leaflet)
+library(plotly)
 
 
 
@@ -23,7 +25,7 @@ shinyServer(function(input, output) {
       print("Getting from data.world")
       query(
         data.world(propsfile = "www/.data.world"),
-        dataset="kurtakranz/s-17-dv-project-6", type="sql",
+        dataset="kurtakranz/s-17-dv-final-project", type="sql",
         query= "SELECT neighbourhood, room_type, avg(price) as average_price,
         SUM(price) as sum_price, count(neighbourhood) as count_neighbourhood
         
@@ -43,7 +45,7 @@ shinyServer(function(input, output) {
     print("Getting from data.world")
     query(
       data.world(propsfile = "www/.data.world"),
-      dataset="kurtakranz/s-17-dv-project-6", type="sql",
+      dataset="kurtakranz/s-17-dv-final-project", type="sql",
       query= "SELECT neighbourhood, room_type, avg(price) as average_price,
       SUM(price) as sum_price, count(neighbourhood) as count_neighbourhood
       
@@ -63,7 +65,7 @@ shinyServer(function(input, output) {
     print("Getting from data.world")
     query(
       data.world(propsfile = "www/.data.world"),
-      dataset="kurtakranz/s-17-dv-project-6", type="sql",
+      dataset="kurtakranz/s-17-dv-final-project", type="sql",
       query= "SELECT neighbourhood, room_type, avg(price) as average_price,
       SUM(price) as sum_price, count(neighbourhood) as count_neighbourhood
       
@@ -94,16 +96,22 @@ shinyServer(function(input, output) {
   )
   })
   
-  output$plot1 <- renderPlot({
-    ggplot(df11(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity') + geom_hline(yintercept=365.8) + labs(title="Entire Room/Apt Prices") + theme_light()
+  output$plot1 <- renderPlotly({
+    p <- ggplot(df11(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity', color = "blue", fill = "lightblue") + geom_hline(yintercept=365.8) + labs(title="Entire Room/Apt Prices") + theme_light() + theme(text = element_text(size=8), axis.text.x = element_text(angle=90, hjust=1))
+    ggplotly(p)
   })
   
-  output$plot2 <- renderPlot({
-    ggplot(df12(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity') + geom_hline(yintercept=105) + labs(title="Private Room Prices") + theme_light()
+  output$plot2 <- renderPlotly({
+    p <- ggplot(df12(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity', color = "blue", fill = "lightblue") + geom_hline(yintercept=105) + labs(title="Private Room Prices") + theme_light() + theme(text = element_text(size=8), axis.text.x = element_text(angle=90, hjust=1))
+    ggplotly(p)
   })
-  output$plot3 <- renderPlot({
-    ggplot(df13(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity') + geom_hline(yintercept=87.9) + labs(title="Shared Room Prices") + theme_light()
+  
+  output$plot3 <- renderPlotly({
+    p <- ggplot(df13(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity', color = "blue", fill = "lightblue") + geom_hline(yintercept=105) + labs(title="Shared Room Prices") + theme_light() + theme(text = element_text(size=8), axis.text.x = element_text(angle=90, hjust=1))
+    ggplotly(p)
   })
+  
+ 
 
 
   
@@ -112,7 +120,7 @@ shinyServer(function(input, output) {
     print("Getting from data.world")
     query(
       data.world(propsfile = "www/.data.world"),
-      dataset="kurtakranz/s-17-dv-project-6", type="sql",
+      dataset="kurtakranz/s-17-dv-final-project", type="sql",
       query= "SELECT neighbourhood, avg(price) as average_price, (avg(price) - 249.7) as price_difference
        
         from clean_listings_summary
@@ -130,8 +138,9 @@ shinyServer(function(input, output) {
   )
   })
   
-  output$barchartPlot1 <- renderPlot({
-    ggplot(df2(), aes(x=factor(neighbourhood), y=price_difference)) + geom_bar(stat = 'identity')
+  output$barchartPlot1 <- renderPlotly({
+    p <- ggplot(df2(), aes(x=factor(neighbourhood), y=price_difference)) + geom_bar(stat = 'identity', color="blue", fill="lightblue") + theme_light() + theme(text = element_text(size=8), axis.text.x = element_text(angle=90, hjust=1))
+    ggplotly(p)
   })
   
   
@@ -140,7 +149,7 @@ shinyServer(function(input, output) {
     print("Getting from data.world")
     query(
       data.world(propsfile = "www/.data.world"),
-      dataset="kurtakranz/s-17-dv-project-6", type="sql",
+      dataset="kurtakranz/s-17-dv-final-project", type="sql",
       query= "SELECT cast(l.neighbourhood as string), p.B05001_001 as population, avg(price) as             average_price
       from clean_listings_summary l join 
       uscensusbureau.`acs-2015-5-e-foreignbirth`.`USA_ZCTA.csv/USA_ZCTA` p
@@ -167,6 +176,126 @@ shinyServer(function(input, output) {
   output$barchartPlot3 <- renderPlot({
     ggplot(df3(), aes(x=factor(neighbourhood), y=average_price)) + geom_bar(stat = 'identity')
   })
+  
+  df_hist <- eventReactive(input$click4, {
+    
+    print("Getting from data.world")
+    query(
+      data.world(propsfile = "www/.data.world"),
+      dataset="kurtakranz/s-17-dv-final-project", type="sql",
+      query= "SELECT price 
+       
+        from clean_listings_summary
+        where price < 4000"
+      
+    ) 
+  }
+  )
+  
+  output$histogramData <- renderDataTable({DT::datatable(df_hist(), rownames = FALSE,
+                                                         extensions = list(Responsive = TRUE, FixedHeader = TRUE)
+  )
+  })
+  
+   output$histogramPlot <- renderPlotly({
+     p <- ggplot(df_hist(), aes(x=price)) +
+       geom_histogram(binwidth = 10, color = "blue") + theme_light()
+     ggplotly(p)
+  })
+   
+   df_scatter <- eventReactive(input$click5, {
+     
+     print("Getting from data.world")
+     query(
+       data.world(propsfile = "www/.data.world"),
+       dataset="kurtakranz/s-17-dv-final-project", type="sql",
+       query= "SELECT p.B05001_001 as population, avg(price) as average_price
+      from clean_listings_summary l join 
+      uscensusbureau.`acs-2015-5-e-foreignbirth`.`USA_ZCTA.csv/USA_ZCTA` p
+      on (l.neighbourhood = p.ZCTA)
+      group by l.neighbourhood"
+       
+     ) 
+   }
+   )
+   
+   output$scatterData <- renderDataTable({DT::datatable(df_scatter(), rownames = FALSE,
+                                                          extensions = list(Responsive = TRUE, FixedHeader = TRUE)
+   )
+   })
+   
+   output$scatterPlot <- renderPlotly({
+     p <- ggplot(df_scatter(), aes(x=population, y=average_price)) +
+       geom_point(color = "skyblue") + geom_smooth(method=lm, se=FALSE) + theme_light()
+     ggplotly(p)
+   })
+   
+   KPI_Low = reactive({input$KPI1})     
+   KPI_High = reactive({input$KPI2})
+   
+   df_kpi <- eventReactive(input$click6, {
+     
+     print("Getting from data.world")
+     query(
+       data.world(propsfile = "www/.data.world"),
+       dataset="kurtakranz/s-17-dv-final-project", type="sql",
+       query= "SELECT neighbourhood, room_type, avg(price) as average_price,
+       SUM(price) as sum_price, count(neighbourhood) as count_neighbourhood,
+       
+       case
+       when (SUM(price) / COUNT(neighbourhood)) < ? then '03 Low'
+       when (SUM(price) / COUNT(neighbourhood)) > ? then '01 High'
+       else '02 Medium'
+       end AS kpi
+       
+       from clean_listings_summary
+       
+       group by neighbourhood, room_type
+       order by neighbourhood",
+        queryParameters = list(KPI_Low(), KPI_High())
+      ) 
+    }
+)
+    
+  output$kpiData <- renderDataTable({DT::datatable(df_kpi(), rownames = FALSE,
+                  extensions = list(Responsive = TRUE, FixedHeader = TRUE)
+  )
+  })
+  
+  output$kpiPlot <- renderPlotly({p <- ggplot(df_kpi()) + 
+      
+      geom_text(aes(x=room_type, y=neighbourhood, label=round(average_price, 2)), size=6) +
+      geom_tile(aes(x=room_type, y=neighbourhood, fill=kpi), alpha=0.50) + theme_light()
+      ggplotly(p)
+  })
 
+  df_box <- eventReactive(input$click7, {
+    
+    print("Getting from data.world")
+    query(
+      data.world(propsfile = "www/.data.world"),
+      dataset="kurtakranz/s-17-dv-final-project", type="sql",
+      query= "SELECT price, cast(neighbourhood as string)
+      
+      from clean_listings_summary
+      where neighbourhood in (78703, 78730, 78732, 78735, 78737)
+      " 
+      
+    ) 
+  }
+    )
+  
+  output$boxplotData <- renderDataTable({DT::datatable(df_box(), rownames = FALSE,
+                                                         extensions = list(Responsive = TRUE, FixedHeader = TRUE)
+  )
+  })
+  
+  output$boxplotPlot <- renderPlotly({
+    p <- ggplot(df_box(), aes(x=factor(neighbourhood), y=price)) +
+      geom_boxplot() + theme_light()
+    ggplotly(p)
+  })
+  
+  
 } 
 )
--- 02 Shiny/Barcharts/ui.R
+++ 02 Shiny/Barcharts/ui.R
@@ -9,14 +9,16 @@ dashboardPage(
   ),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Room Type Price", tabName = "barchart1", icon = icon("dashboard")),
       menuItem("Above or Below Average", tabName = "barchart2", icon = icon("dashboard")),
-      menuItem("Population vs. Average Price", tabName = "barchart3", icon = icon("dashboard"))
+      menuItem("Room Type Price", tabName = "barchart1", icon = icon("dashboard")),
+      menuItem("Price Histogram", tabName = "histogram", icon = icon("dashboard")),
+      menuItem("Population vs. Average Price", tabName = "scatter", icon = icon("dashboard")),
+      menuItem("Average Price KPI", tabName = "kpi_table", icon = icon("dashboard")),
+      menuItem("Boxplot", tabName = "boxplot", icon = icon("dashboard"))
     )
   ),
   dashboardBody(    
     tabItems(
-      # Begin Crosstab tab content.
       tabItem(tabName = "barchart1",
               tabsetPanel(
                 tabPanel("Data",  
@@ -29,9 +31,9 @@ dashboardPage(
                          hr(),
                          DT::dataTableOutput("data3")
                 ),
-                tabPanel("Barcharts", plotOutput("plot1", height=250), 
-                                      plotOutput("plot2", height=250),
-                                      plotOutput("plot3", height=250))
+                tabPanel("Barcharts", plotlyOutput("plot1", height=350), 
+                                      plotlyOutput("plot2", height=350),
+                                      plotlyOutput("plot3", height=350))
               )
       ),
      tabItem(tabName = "barchart2",
@@ -43,20 +45,63 @@ dashboardPage(
                            
                 
             ),
-                  tabPanel("Barchart", plotOutput("barchartPlot1", height=500))
+                  tabPanel("Barchart", plotlyOutput("barchartPlot1", height=500))
                 )
       ),
-     tabItem(tabName = "barchart3",
-                tabsetPanel(
-                  tabPanel("Data",
-                           actionButton(inputId = "click3", label = "To get data, click here"), 
-                           hr(),
-                           DT::dataTableOutput("barchartData2")
-                           
-                           ),
-                  tabPanel("Barcharts", plotOutput("barchartPlot2", height=300),                                  plotOutput("barchartPlot3", height=300))
-                )
-    )  
+     tabItem(tabName = "histogram",
+             tabsetPanel(
+               tabPanel("Data",  
+                        actionButton(inputId = "click4",  label = "To get data, click here"),
+                        hr(), # Add space after button 
+                        DT::dataTableOutput("histogramData")
+                        
+                        
+               ),
+               tabPanel("Histogram", plotlyOutput("histogramPlot", height=500))
+             )
+     ),
+     tabItem(tabName = "scatter",
+             tabsetPanel(
+               tabPanel("Data",  
+                        actionButton(inputId = "click5",  label = "To get data, click here"),
+                        hr(), # Add space after button 
+                        DT::dataTableOutput("scatterData")
+                        
+                        
+               ),
+               tabPanel("Scatter Plot", plotlyOutput("scatterPlot", height=500))
+             )
+     ),
+     tabItem(tabName = "kpi_table",
+             tabsetPanel(
+               tabPanel("Data",  
+                        sliderInput("KPI1", "Average Price - Low:", 
+                                    min = 0, max = 286,  value = 286),
+                        sliderInput("KPI2", "Average Price - High:", 
+                                    min = 286.01, max = 800,  value = 800),
+                        actionButton(inputId = "click6",  label = "To get data, click here"),
+                        hr(), # Add space after button 
+                        DT::dataTableOutput("kpiData")
+                        
+                        
+               ),
+               tabPanel("KPI Table", plotlyOutput("kpiPlot", height=1500))
+             )
+     ),
+     tabItem(tabName = "boxplot",
+             tabsetPanel(
+               tabPanel("Data",  
+                        actionButton(inputId = "click7",  label = "To get data, click here"),
+                        hr(), # Add space after button 
+                        DT::dataTableOutput("boxplotData")
+                        
+                        
+               ),
+               tabPanel("Boxplot", plotlyOutput("boxplotPlot", height=500))
+             )
+     )
+    
+      
   )
 )    
 )
